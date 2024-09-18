fac8a6cd337cb406fe83cd956a1ab82e574832b4
Jamie Kennedy <jamie.kennedy@capgemini.com>
2024-09-18 12:00:29 +0100

    Run Spring Initializr to generate boiler-plate code

---
+++ src/main/java/com/ddes/smart_meter_system_back_end/SmartMeterSystemBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.ddes.smart_meter_system_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMeterSystemBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartMeterSystemBackEndApplication.class, args);
+	}
+
+}
