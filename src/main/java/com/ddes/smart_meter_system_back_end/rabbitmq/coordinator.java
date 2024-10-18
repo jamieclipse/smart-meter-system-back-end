f221f09a3f2a7de75eac28c1140ece6136bc39c8
Del-Cap <97227804+Del-Cap@users.noreply.github.com>
2024-10-18 14:48:11 +0100

    added stubs for new classes

---
+++ src/main/java/com/ddes/smart_meter_system_back_end/rabbitmq/coordinator.java
@@ -0,0 +1,20 @@
+package com.ddes.smart_meter_system_back_end.rabbitmq;
+
+
+public class coordinator {
+
+    public coordinator (){
+
+    }
+
+    public void getReading(){
+
+       // return meterID, readingTimestamp, readingValue
+    }
+    
+    public void sendBill(){
+    //add following params meterID, billTimestamp, billValue, dayRate
+      //  return success message
+    }
+    
+}
