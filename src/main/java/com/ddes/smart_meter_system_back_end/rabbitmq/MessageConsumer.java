dbc69f20096f4d0b11f3b20d605d8092e1a3294f
Jamie Kennedy <jamie.kennedy@capgemini.com>
2024-10-22 17:15:49 +0100

    Remove compose.yaml & unneeded dependencies, tweak baseline RabbitMQ setup

--- src/main/java/com/ddes/smart_meter_system_back_end/rabbitmq/MessageConsumer.java
+++ src/main/java/com/ddes/smart_meter_system_back_end/rabbitmq/MessageConsumer.java
@@ -1,14 +1,17 @@
 package com.ddes.smart_meter_system_back_end.rabbitmq;
 
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageConsumer {
 
-    @RabbitListener(queues = "queue")
-    public void receiveMessage(String message){
-        System.out.println(message);
+    @RabbitListener(queues = "server.inbound")
+    public void receiveMessage(Message message){
+        System.out.println("Message received with following properties: " + message.getMessageProperties());
+        System.out.println("Message received with following payload: " + new String(message.getBody()));
+        // TODO: Convert this to proper logging
     }
 
 }
