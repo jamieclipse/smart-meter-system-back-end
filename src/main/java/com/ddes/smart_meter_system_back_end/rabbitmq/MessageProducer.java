231a8d3af4c3520dc12228d2456a0f27e2e1b341
Jamie Kennedy <gigjsk@gmail.com>
2024-11-14 12:00:00 +0000

    Stub MessageProducer

--- src/main/java/com/ddes/smart_meter_system_back_end/rabbitmq/MessageProducer.java
+++ src/main/java/com/ddes/smart_meter_system_back_end/rabbitmq/MessageProducer.java
@@ -1,14 +1,18 @@
 package com.ddes.smart_meter_system_back_end.rabbitmq;
 
+import java.util.logging.Logger;
+
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class MessageProducer {
+    Logger log = Logger.getLogger(MessageConsumer.class.getName());
 
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
     public void sendMessage(String meterId, double billAmount){
-        rabbitTemplate.convertAndSend("server.outbound", "Meter ID: " + meterId + " Bill Amount: " + billAmount);
+        //rabbitTemplate.convertAndSend("server.outbound", "Meter ID: " + meterId + " Bill Amount: " + billAmount);
+        log.info("Message is ready to send.");
     }
 }
