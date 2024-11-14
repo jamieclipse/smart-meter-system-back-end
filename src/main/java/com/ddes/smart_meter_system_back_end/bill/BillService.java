bbe077fbd0623bdaa7d57ba2185a4ec07594a282
Jamie Kennedy <gigjsk@gmail.com>
2024-11-14 10:53:31 +0000

    chore: Add VAT

--- src/main/java/com/ddes/smart_meter_system_back_end/bill/BillService.java
+++ src/main/java/com/ddes/smart_meter_system_back_end/bill/BillService.java
@@ -14,7 +14,8 @@ public class BillService {
         double difference = rs.calculateReadingDifference(reading.getMeterId(), reading.getValue());
         double tariff = 0.21; //TODO: get the tariff from somewhere else
         double standingCharge = 0.60; //TODO: get the standing charge from somewhere else
-        double bill = (difference * tariff) + standingCharge;
+        double VAT = 1.05; //TODO: get the VAT from somewhere else
+        double bill = ((difference * tariff) + standingCharge) * VAT;
         return bill;
     }
 }
