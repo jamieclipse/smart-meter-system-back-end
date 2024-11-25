51afa4fd332035452230aa9b869bdd8ab053ffcd
Jamie Kennedy <jamie.kennedy@capgemini.com>
2024-11-25 20:43:20 +0000

    chore: Initial JPA Repository setup

--- src/main/java/com/ddes/smart_meter_system_back_end/bill/BillRepository.java
+++ src/main/java/com/ddes/smart_meter_system_back_end/bill/BillRepository.java
@@ -1,9 +1,9 @@
 package com.ddes.smart_meter_system_back_end.bill;
 
-import org.springframework.data.repository.CrudRepository;
-
 import java.util.List;
 
-public interface BillRepository extends CrudRepository<Bill, Long> {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BillRepository extends JpaRepository<Bill, Long> {
     List<Bill> findByMeterId(String meterId);
 }
--- src/main/java/com/ddes/smart_meter_system_back_end/reading/ReadingRepository.java
+++ src/main/java/com/ddes/smart_meter_system_back_end/reading/ReadingRepository.java
@@ -1,10 +1,10 @@
 package com.ddes.smart_meter_system_back_end.reading;
 
-import org.springframework.data.repository.CrudRepository;
-
 import java.util.List;
 
-public interface ReadingRepository extends CrudRepository<Reading, Long> {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReadingRepository extends JpaRepository<Reading, Long> {
     List<Reading> findByMeterId(String meterId);
     
 }
