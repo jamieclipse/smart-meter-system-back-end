208d72909ba13aa480cd6e1b2260963b6a60ee4f
Jamie Kennedy <jamie.kennedy@capgemini.com>
2024-11-30 16:48:36 +0000

    chore: Add logging for initial reading extraction

--- src/main/java/com/ddes/smart_meter_system_back_end/reading/ReadingService.java
+++ src/main/java/com/ddes/smart_meter_system_back_end/reading/ReadingService.java
@@ -1,18 +1,21 @@
 package com.ddes.smart_meter_system_back_end.reading;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReadingService {
+    Logger log = Logger.getLogger(ReadingService.class.getName());
 
     @Autowired
     private ReadingRepository readingRepository;
     
     public double calculateReadingDifference(String clientId, double newReading){
         double initialReading = getInitialReading(clientId);
+        log.info("Successfully extracted initial reading: " + initialReading);
         double difference = newReading - initialReading; // New reading should always be greater than initial reading
         return difference;
 
